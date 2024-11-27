8ce06c93584da21458b6b731ed62e5721f109f05
SaseQ <bmarczuk03@gmail.com>
2024-11-27 19:23:22 +0100

    Refactor Ping command;

--- src/main/java/co/freddev/javajdatemplate/bot/command/Ping.java
+++ src/main/java/co/freddev/javajdatemplate/bot/command/Ping.java
@@ -5,8 +5,6 @@ import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
 
 @Component
@@ -20,10 +18,8 @@ public class Ping extends SlashCommand {
     @Override
     protected void execute(SlashCommandEvent event) {
         event.reply("Ping: ...").queue(m -> {
-            final OffsetDateTime offsetDateTime = Instant.now().atOffset(ZoneOffset.ofTotalSeconds((int) (2.0 * 3600)));
-            final long ping = event.getTimeCreated().until(offsetDateTime, ChronoUnit.MILLIS);
-            final long gatewayPing = event.getJDA().getGatewayPing();
-            m.editOriginal("Pong! "+ ping  + "ms | Websocket: " + gatewayPing + "ms").queue();
+            long ping = event.getTimeCreated().toInstant().until(Instant.now(), ChronoUnit.MILLIS);
+            m.editOriginal("Pong! " + ping + "ms | Websocket: " + event.getJDA().getGatewayPing() + "ms").queue();
         });
     }
 }
